a8e38b5c1ef59c31788f377d1ebe31bced93ecd7
AbhiTheModder <allinoneallinone00@gmail.com>
2023-02-13 14:41:08 +0530

    afk fix

--- modules/afk.py
+++ modules/afk.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from userbot.helpers.PyroHelpers import GetChatID, ReplyCheck
+
 from pyrogram import Client, filters
 
 from utils.misc import modules_help, prefix
