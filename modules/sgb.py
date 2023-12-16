15f02faeec6336d09b9060e345c79763f9785639
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2023-12-16 09:52:00 +0000

    Fix: sgb.py
    Remove support for reply and fix loop

--- modules/sgb.py
+++ modules/sgb.py
@@ -4,33 +4,29 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 from pyrogram.errors import RPCError
 
-from utils.misc import modules_help, prefix
 
-async def edit_or_reply(message, text, parse_mode=enums.ParseMode.MARKDOWN):
-    """Edit Message If Its From Self, Else Reply To Message, (Only Works For Sudo's)"""
-    if not message:
-        return await message.edit(text, parse_mode=parse_mode)
-    if not message.from_user:
-        return await message.edit(text, parse_mode=parse_mode)
-    return await message.edit(text, parse_mode=parse_mode)
+from utils.misc import modules_help, prefix
+from utils.scripts import edit_or_reply
 
 
 @Client.on_message(filters.command("sgb", prefix) & filters.me)
 async def sg(client: Client, message: Message):
     lol = await edit_or_reply(message, "`Processing please wait`", parse_mode=enums.ParseMode.MARKDOWN)
-    if not message.reply_to_message:
-        await lol.edit("`reply to any message`", parse_mode=enums.ParseMode.MARKDOWN)
-    reply = message.reply_to_message
-    if not reply.text:
-        await lol.edit("`reply to any text message`", parse_mode=enums.ParseMode.MARKDOWN)
+    if len(message.command) > 1:
+        reply = message.text.split(maxsplit=1)[1]
+    elif message.reply_to_message:
+        reply = message.reply_to_message.text
+    else:
+        await message.edit(f"<b>Usage: </b><code>{prefix}sgb [id]</code>", parse_mode=enums.ParseMode.HTML)
+        return
     chat = message.chat.id
     try:
         await client.send_message("@SangMata_beta_bot","/start", parse_mode=enums.ParseMode.MARKDOWN)
     except RPCError:
         await lol.edit("**Please unblock @SangMata_beta_bot and try again**", parse_mode=enums.ParseMode.MARKDOWN)
         return
-    
-    await reply.forward("@SangMata_beta_bot")
+    id = "@SangMata_beta_bot"
+    await client.send_message(id, reply, parse_mode=enums.ParseMode.MARKDOWN)
     await asyncio.sleep(2)
     async for opt in client.get_chat_history("@SangMata_beta_bot", limit=1):
         hmm = opt.text
