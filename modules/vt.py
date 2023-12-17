706f055815c2d14229012cc7f519cf3fb1876e2c
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2023-12-17 23:11:49 +0530

    Update vt.py

--- modules/vt.py
+++ modules/vt.py
@@ -19,34 +19,29 @@ from utils.config import vt_key as vak
 
 @Client.on_message(filters.command("vt", prefix) & filters.me)
 async def scan_my_file(client, message):
-    try:
-        ms_ = await edit_or_reply(message, "`Please Wait! Scanning This File`", parse_mode=enums.ParseMode.MARKDOWN)
-        if not message.reply_to_message:
-            return await ms_.edit("`Please Reply To File To Scan For Viruses`", parse_mode=enums.ParseMode.MARKDOWN)
-        if not message.reply_to_message.document:
-            return await ms_.edit("`Please Reply To File To Scan For Viruses`", parse_mode=enums.ParseMode.MARKDOWN)
-        if not vak:
-            return await ms_.edit("`You Need To Set VIRUSTOTAL_API_KEY For Functing Of This Plugin.`", parse_mode=enums.ParseMode.MARKDOWN)
-        if int(message.reply_to_message.document.file_size) > 250000000:
-            return await ms_.edit("`File Too Large , Limit is 25 Mb`", parse_mode=enums.ParseMode.MARKDOWN)
-        c_time = time.time()
-        downloaded_file_name = await message.reply_to_message.download(
-            progress=progress,
-            progress_args=(ms_, c_time, '`Downloading This File!`'),
+    ms_ = await edit_or_reply(message, "`Please Wait! Scanning This File`")
+    if not message.reply_to_message:
+      return await ms_.edit("`Please Reply To File To Scan For Viruses`")
+    if not message.reply_to_message.document:
+      return await ms_.edit("`Please Reply To File To Scan For Viruses`")
+    if not vak:
+      return await ms_.edit("`You Need To Set VIRUSTOTAL_API_KEY For Functing Of This Plugin.`")
+    if int(message.reply_to_message.document.file_size) > 25000000:
+      return await ms_.edit("`File Too Large , Limit is 25 Mb`")
+    c_time = time.time()
+    downloaded_file_name = await message.reply_to_message.download(
+        progress=progress,
+        progress_args=(ms_, c_time, '`Downloading This File!`'),
     )
 
-        url = "https://www.virustotal.com/vtapi/v2/file/scan"
-        params = {"apikey": vak}
-        files = {"file": (downloaded_file_name, open(downloaded_file_name, "rb"))}
-        response = requests.post(url, files=files, params=params)
-        try:
-           r_json = response.json()
-           scanned_url = r_json["permalink"]
-        except:
-            return await ms_.edit(f"`[{response.status_code}] - Unable To Scan File.`", parse_mode=enums.ParseMode.MARKDOWN)
-        await ms_.edit(f"<b><u>Scanned {message.reply_to_message.document.file_name}</b></u>. <b>You Can Visit :</b> <a href=\"{scanned_url}\">Here</a> <b>In 5-10 Min To See File Report</b>")
-
-    except Exception as e:
-            await message.reply_text(f"An error occurred: {format_exc(e)}")
-    finally:
-        os.remove(downloaded_file_name)
+    url = "https://www.virustotal.com/vtapi/v2/file/scan"
+    params = {"apikey": vak}
+    files = {"file": (downloaded_file_name, open(downloaded_file_name, "rb"))}
+    response = requests.post(url, files=files, params=params)
+    try:
+       r_json = response.json()
+       scanned_url = r_json["permalink"]
+    except:
+      return await ms_.edit(f"`[{response.status_code}] - Unable To Scan File.`")
+    await ms_.edit(f"<b><u>Scanned {message.reply_to_message.document.file_name}</b></u>. <b>You Can Visit :</b> <a href=\"{scanned_url}\">Here</a> <b>In 5-10 Min To See File Report</b>")
+    os.remove(downloaded_file_name)
