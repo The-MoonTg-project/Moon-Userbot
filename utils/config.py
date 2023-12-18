c4b1d6dd49cc5c97c481fdcaf4c3b1df90d8e29f
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2023-12-18 23:10:55 +0530

    Update config.py
    Add: gemini_key for Ai models

--- utils/config.py
+++ utils/config.py
@@ -13,5 +13,7 @@ db_name = env.str("DATABASE_NAME")
 apiflash_key = env.str("APIFLASH_KEY")
 rmbg_key = env.str("RMBG_KEY")
 vt_key = env.str("VT_KEY")
+gemini_key = env.str("GEMINI_KEY")
+
 test_server = env.bool("TEST_SERVER", False)
 modules_repo_branch = env.str("MODULES_REPO_BRANCH", "master")
