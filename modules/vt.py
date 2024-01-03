da7f23f81adff9baaf88d11dabda7b4408486d3c
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-01-03 15:17:17 +0000

    Try: Increase limit

--- modules/vt.py
+++ modules/vt.py
@@ -6,6 +6,7 @@
 #
 # All rights reserved.
 
+import json
 import os
 import time
 import requests
@@ -24,10 +25,10 @@ async def scan_my_file(client, message):
       return await ms_.edit("`Please Reply To File To Scan For Viruses`", parse_mode=enums.ParseMode.MARKDOWN)
     if not message.reply_to_message.document:
       return await ms_.edit("`Please Reply To File To Scan For Viruses`", parse_mode=enums.ParseMode.MARKDOWN)
-    if not vak:
+    if vak == None:
       return await ms_.edit("`You Need To Set VIRUSTOTAL_API_KEY For Functing Of This Plugin.`", parse_mode=enums.ParseMode.MARKDOWN)
-    if int(message.reply_to_message.document.file_size) > 25000000:
-      return await ms_.edit("`File Too Large , Limit is 25 Mb`", parse_mode=enums.ParseMode.MARKDOWN)
+    if int(message.reply_to_message.document.file_size) > 32000000:
+      return await ms_.edit("**File Too Large, Use `.vtl` instead**", parse_mode=enums.ParseMode.MARKDOWN)
     c_time = time.time()
     downloaded_file_name = await message.reply_to_message.download(
         progress=progress,
@@ -39,9 +40,63 @@ async def scan_my_file(client, message):
     files = {"file": (downloaded_file_name, open(downloaded_file_name, "rb"))}
     response = requests.post(url, files=files, params=params)
     try:
+      #  result = response.text
+      #  print(result)
        r_json = response.json()
-       scanned_url = r_json["permalink"]
-    except:
-      return await ms_.edit(f"`[{response.status_code}] - Unable To Scan File.`")
-    await ms_.edit(f"<b><u>Scanned {message.reply_to_message.document.file_name}</b></u>\n<b>You Can Visit : </b><a href='{scanned_url}'>Here</a>\n<b>In 5-10 Min To See File Report</b>")
+       md5 = r_json["md5"]
+      #  print(md5)
+    except Exception as e:
+      return await ms_.edit(format_exc(e))
+    await ms_.edit(f"<b><u>Scanned {message.reply_to_message.document.file_name}</b></u>. <b>You Can Visit :</b> <a href=\"https://www.virustotal.com/gui/file/{md5}\">Here</a> <b>In 5-10 Min To See File Report</b>")
+    os.remove(downloaded_file_name)
+
+@Client.on_message(filters.command("vtl", prefix) & filters.me)
+async def scan_my_large_file(client, message):
+    ms_ = await edit_or_reply(message, "`Please Wait! Scanning This File`")
+    if not message.reply_to_message:
+      return await ms_.edit("`Please Reply To File To Scan For Viruses`", parse_mode=enums.ParseMode.MARKDOWN)
+    if not message.reply_to_message.document:
+      return await ms_.edit("`Please Reply To File To Scan For Viruses`", parse_mode=enums.ParseMode.MARKDOWN)
+    if vak == None:
+      return await ms_.edit("`You Need To Set VIRUSTOTAL_API_KEY For Functing Of This Plugin.`", parse_mode=enums.ParseMode.MARKDOWN)
+    if int(message.reply_to_message.document.file_size) > 650000000:
+      return await ms_.edit("**File Too Large, exceeded Max capacity of 650MB**", parse_mode=enums.ParseMode.MARKDOWN)
+    c_time = time.time()
+    downloaded_file_name = await message.reply_to_message.download(
+        progress=progress,
+        progress_args=(ms_, c_time, '`Downloading This File!`'),
+    )
+
+    url1 = "https://www.virustotal.com/api/v3/files/upload_url"
+
+    headers = {
+        "accept": "application/json",
+        "x-apikey": vak
+    }
+
+    rponse = requests.get(url1, headers=headers)
+    try:
+      # result = rponse.text
+      #  print(result)
+       r_json = rponse.json()
+       upl_data = r_json["data"]
+       print(upl_data)
+    except Exception as e:
+      return await ms_.edit(format_exc(e))
+
+    # print(rponse.text)
+
+    url = upl_data
+    params = {"apikey": vak}
+    files = {"file": (downloaded_file_name, open(downloaded_file_name, "rb"))}
+    response = requests.post(url, files=files, params=params)
+    try:
+      #  result = response.text
+      #  print(result)
+       r_json = response.json()
+       md5 = r_json["md5"]
+      #  print(md5)
+    except Exception as e:
+      return await ms_.edit(format_exc(e))
+    await ms_.edit(f"<b><u>Scanned {message.reply_to_message.document.file_name}</b></u>. <b>You Can Visit :</b> <a href=\"https://www.virustotal.com/gui/file/{md5}\">Here</a> <b>In 5-10 Min To See File Report</b>")
     os.remove(downloaded_file_name)
