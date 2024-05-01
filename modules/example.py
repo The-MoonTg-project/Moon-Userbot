e1b4b5c58ff4ea739a8c7764a3d3d72d4b27fda7
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2024-05-02 00:02:39 +0530

    Update example.py

--- modules/example.py
+++ modules/example.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from pyrogram import Client, filters
+from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 
 from utils.misc import modules_help, prefix
