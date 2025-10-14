bcb7e6d98f7adb1f54417c1b6b07b707c4fa5d26
Abhi <85984486+AbhiTheModder@users.noreply.github.com>
2025-10-14 18:33:52 +0530

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+BIND_ADDR="0.0.0.0:${PORT:-8000}"
+
 cat <<'EOF'
  _      ____  ____  _     
 / \__/|/  _ \/  _ \/ \  /|
@@ -7,11 +9,12 @@ cat <<'EOF'
 | |  ||| \_/|| \_/|| | \||
 \_/  \|\____/\____/\_/  \|
                           
-Copyright (C) 2020-2023 by MoonTg-project@Github, < https://github.com/The-MoonTg-project >.
+Copyright (C) 2020-2025 by MoonTg-project@Github, < https://github.com/The-MoonTg-project >.
 This file is part of < https://github.com/The-MoonTg-project/Moon-Userbot > project,
 and is released under the "GNU v3.0 License Agreement".
 Please see < https://github.com/The-MoonTg-project/Moon-Userbot/blob/main/LICENSE >
 All rights reserved.
 EOF
 
-gunicorn app:app --daemon && python main.py
+echo "Starting gunicorn server on $BIND_ADDR..."
+gunicorn app:app --daemon --bind "$BIND_ADDR" && python main.py
